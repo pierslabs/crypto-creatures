a42b5663f7bab254af76e5814b540984169c9376
pierslabs <pedrolosasp@gmail.com>
2023-12-04 21:40:13 +0100

    add gold-balance tests

--- src/gold-balance/gold-balance.controller.spec.ts
+++ src/gold-balance/gold-balance.controller.spec.ts
@@ -1,20 +1,76 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { User } from 'src/users/schema/users.schema';
 import { GoldBalanceController } from './gold-balance.controller';
 import { GoldBalanceService } from './gold-balance.service';
+import { Role } from 'src/users/enums/roles.enum';
 
 describe('GoldBalanceController', () => {
   let controller: GoldBalanceController;
+  let service: GoldBalanceService;
+
+  const user: User = {
+    name: 'Jhon Doe',
+    role: ['user'] as Role[],
+    password: '$2b$10$GNfcHvlvtb/fR1IiekCzm.47iMthJbjhRAt6ZZMRbwOx9IshKpABC',
+    transactions: [],
+    _id: '656e0ecbd86c4db94994dbfd',
+  };
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [GoldBalanceController],
-      providers: [GoldBalanceService],
-    }).compile();
-
-    controller = module.get<GoldBalanceController>(GoldBalanceController);
+    service = { addGold: jest.fn() } as any;
+    controller = new GoldBalanceController(service);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should add gold', async () => {
+    const creatureId = '1';
+
+    const createTransactionDto = { amount: 10 };
+
+    (service.addGold as jest.Mock).mockResolvedValue({
+      newBalance: 20,
+      message: 'Gold added to Test',
+    });
+
+    const result = await controller.addGold(
+      user,
+      creatureId,
+      createTransactionDto,
+    );
+
+    expect(result).toEqual({
+      newBalance: 20,
+      message: 'Gold added to Test',
+    });
+    expect(service.addGold).toHaveBeenCalledWith(
+      creatureId,
+      user,
+      createTransactionDto,
+    );
+  });
+
+  it('should remove gold', async () => {
+    const creatureId = '1';
+
+    const createTransactionDto = { amount: 10 };
+
+    service.removeGold = jest.fn().mockResolvedValue({
+      newBalance: 0,
+      message: 'Gold removed from Test',
+    });
+
+    const result = await controller.removeGold(
+      user,
+      creatureId,
+      createTransactionDto,
+    );
+
+    expect(result).toEqual({
+      newBalance: 0,
+      message: 'Gold removed from Test',
+    });
+    expect(service.removeGold).toHaveBeenCalledWith(
+      user,
+      creatureId,
+      createTransactionDto,
+    );
   });
 });
--- src/gold-balance/gold-balance.service.spec.ts
+++ src/gold-balance/gold-balance.service.spec.ts
@@ -1,18 +1,90 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { User } from 'src/users/schema/users.schema';
 import { GoldBalanceService } from './gold-balance.service';
 
 describe('GoldBalanceService', () => {
   let service: GoldBalanceService;
+  let mockTransactionModel: any;
+  let mockMysticCreaturesService: any;
+  let mockUsersService: any;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [GoldBalanceService],
-    }).compile();
+    mockTransactionModel = {
+      create: jest.fn(),
+    };
+    mockMysticCreaturesService = {
+      findOne: jest.fn(),
+      update: jest.fn(),
+    };
+    mockUsersService = {
+      updateTransactions: jest.fn(),
+    };
 
-    service = module.get<GoldBalanceService>(GoldBalanceService);
+    service = new GoldBalanceService(
+      mockTransactionModel,
+      mockMysticCreaturesService,
+      mockUsersService,
+    );
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should add gold', async () => {
+    const creatureId = '1';
+    const user = { _id: '1' } as User;
+    const createTransactionDto = { amount: 10 };
+
+    mockMysticCreaturesService.findOne.mockResolvedValue({
+      _id: creatureId,
+      goldBalance: 0,
+      name: { firstName: 'Test' },
+    });
+    mockTransactionModel.create.mockResolvedValue({ _id: 'transaction1' });
+
+    const result = await service.addGold(
+      creatureId,
+      user,
+      createTransactionDto,
+    );
+
+    expect(result).toEqual({
+      newBalance: 10,
+      message: 'Gold added to Test',
+    });
+    expect(mockMysticCreaturesService.update).toHaveBeenCalledWith(creatureId, {
+      goldBalance: 10,
+    });
+    expect(mockUsersService.updateTransactions).toHaveBeenCalledWith(
+      user._id,
+      'transaction1',
+    );
+  });
+
+  it('should remove gold', async () => {
+    const creatureId = '1';
+    const user = { _id: '1' } as User;
+    const createTransactionDto = { amount: 10 };
+
+    mockMysticCreaturesService.findOne.mockResolvedValue({
+      _id: creatureId,
+      goldBalance: 20,
+      name: { firstName: 'Test' },
+    });
+    mockTransactionModel.create.mockResolvedValue({ _id: 'transaction1' });
+
+    const result = await service.removeGold(
+      user,
+      creatureId,
+      createTransactionDto,
+    );
+
+    expect(result).toEqual({
+      newBalance: 10,
+      message: 'Gold remove to Test',
+    });
+    expect(mockMysticCreaturesService.update).toHaveBeenCalledWith(creatureId, {
+      goldBalance: 10,
+    });
+    expect(mockUsersService.updateTransactions).toHaveBeenCalledWith(
+      user._id,
+      'transaction1',
+    );
   });
 });
