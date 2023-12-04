d243a156ed08f9e98a24d57c2b6c395e4bd545f8
pierslabs <pedrolosasp@gmail.com>
2023-12-04 22:01:29 +0100

    add Auth tests

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,20 +1,39 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let service: AuthService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController],
-      providers: [AuthService],
-    }).compile();
-
-    controller = module.get<AuthController>(AuthController);
+    service = { register: jest.fn(), login: jest.fn() } as any;
+    controller = new AuthController(service);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should register a user', async () => {
+    const createUserDto: CreateUserDto = {
+      name: 'Dummy',
+      password: '123456',
+    };
+
+    (service.register as jest.Mock).mockResolvedValue('user registered');
+
+    const result = await controller.create(createUserDto);
+
+    expect(result).toBe('user registered');
+    expect(service.register).toHaveBeenCalledWith(createUserDto);
+  });
+
+  it('should login a user', async () => {
+    const loginDto: LoginDto = { name: 'test', password: 'test' };
+
+    (service.login as jest.Mock).mockResolvedValue('user logged in');
+
+    const result = await controller.login(loginDto);
+
+    expect(result).toBe('user logged in');
+    expect(service.login).toHaveBeenCalledWith(loginDto);
   });
 });
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,82 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import * as bcrypt from 'bcrypt';
+import { LoginDto } from './dto/login.dto';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let mockUsersService: any;
+  let mockJwtService: any;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+    mockUsersService = {
+      findOne: jest.fn(),
+      create: jest.fn(),
+      findOneByName: jest.fn(),
+    };
+    mockJwtService = {
+      sign: jest.fn(),
+    };
 
-    service = module.get<AuthService>(AuthService);
+    service = new AuthService(mockUsersService, mockJwtService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should login a user', async () => {
+    const loginDto: LoginDto = { name: 'test', password: 'test' };
+
+    mockUsersService.findOneByName = jest.fn().mockResolvedValue([
+      {
+        _id: '1',
+        name: 'test',
+        password: bcrypt.hashSync('test', 10),
+        role: 'user',
+      },
+    ]);
+
+    mockJwtService.sign.mockReturnValue('token');
+
+    const result = await service.login(loginDto);
+
+    expect(result).toEqual({ token: 'token' });
+  });
+
+  it('should register a user', async () => {
+    const createUserDto: CreateUserDto = { name: 'test', password: 'test' };
+
+    mockUsersService.create.mockResolvedValue({ _id: '1' });
+    mockJwtService.sign.mockReturnValue('token');
+
+    const result = await service.register(createUserDto);
+
+    expect(result).toEqual({ token: 'token' });
+  });
+
+  it('should validate a user', async () => {
+    const userId = '1';
+
+    const user = mockUsersService.findOne.mockResolvedValue({
+      _id: userId,
+      role: 'user',
+      name: 'test',
+      password: 'test',
+    });
+
+    delete user.password;
+
+    const result = await service.validateUser(userId);
+
+    expect(result).toEqual({
+      _id: userId,
+      role: 'user',
+      name: 'test',
+    });
+  });
+
+  it('should check if a day is a weekday', () => {
+    const dayNumber = 1;
+
+    const result = service.isDayOfWeek(dayNumber);
+
+    expect(result).toBe(true);
   });
 });
